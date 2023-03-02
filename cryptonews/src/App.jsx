5ffa205878926d6312a6b7437d39db9171a7a9da
ar78studio <info@ar78.studio>
2023-03-02 20:04:43 +0100

    CryptoNews - setting initial structure

---
+++ cryptonews/src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+// import { Route, Switch, Link } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, Link } from "react-router";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+        <div className='navbar'>
+          <Navbar />
+        </div>
+        <div className='main'>
+          <Layout>
+            <div className='routes'>
+              <Switch>
+                <Route exact path='/'>
+                  <Homepage />
+                </Route>
+                <Route exact path='/exchanges'>
+                  <Exchanges />
+                </Route>
+                <Route exact path='/cryptocurrencies'>
+                  <Cryptocurrencies />
+                </Route>
+                <Route exact path='/crypto/:coinId'>
+                  <CryptoDetails />
+                </Route>
+                <Route exact path='/news'>
+                  <News />
+                </Route>
+              </Switch>
+            </div>
+          </Layout>
+        </div>
+        <div className='footer'></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
